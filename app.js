86e195240cf330ce2b3cc78008f24228a212210c
mahmoudmohamedsh <201727100@std.sci.cu.edu.eg>
2022-07-18 01:28:23 +0200

    fix error

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const app = express();
 //allow headers and methods
 app.use((req,res,next)=>{
 res.setHeader('Access-Control-Allow-Origin','*');
-res.setHeader('Access-Control-Allow-Methons','OPTIONS, GET, POST, PUT,PATCH,DELETE');
+res.setHeader('Access-Control-Allow-Methons','OPTIONS, GET, POST, PUT, PATCH, DELETE');
 res.setHeader('Access-Control-Allow-Headers','Content-Type , Authorization');
+next();
 });
 //example of token : 'Bearer sdkfjslkjflsdjfl' 
 
@@ -25,6 +26,6 @@ app.use((error,req,res,next)=>{
 });
 
 mongoose.connect('mongodb+srv://sharp2:dnw0fx60ldLKCgu2@cluster0.o5no6.mongodb.net/?retryWrites=true&w=majority').then(result=>{
-    app.listen(process.env.PORT || 8080); 
+    app.listen( 8080); 
     console.log('done server running');
 }).catch(err=>console.log(err)); 
